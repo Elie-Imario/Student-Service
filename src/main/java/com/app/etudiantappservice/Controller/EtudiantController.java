154b2dfbe7bb601585a211ab5e93e73b366ad549
elie-imario <elieimario1@gmail.com>
2024-05-27 11:03:20 +0300

    change endpoints

--- src/main/java/com/app/etudiantappservice/Controller/EtudiantController.java
+++ src/main/java/com/app/etudiantappservice/Controller/EtudiantController.java
@@ -11,7 +11,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 @RestController
-@RequestMapping("/")
+@RequestMapping("/api")
 public class EtudiantController {
     @Autowired
     EtudiantService etudiantService;
